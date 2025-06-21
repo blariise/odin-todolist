2e9ff41898537a5dddc591325d441b507fa63a32
blarrise <61951559+blariise@users.noreply.github.com>
2025-06-21 03:29:14 +0200

    Add add icon and simple html input and styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css";
-import Task from "./task.js";
 
+import Task from "./task.js";
+import TodoList from "./todolist.js";
+
+import addIcon from "./add.svg";
+
+/*
+ * INIT PAGE
+*/
+(() => {
+  const defaultList = new TodoList("default");
+
+  initIcons();
+})();
+
+function initIcons() {
+  const addIconDivs = document.querySelectorAll(".add-icon");
+  addIconDivs.forEach((addIconDiv) => {
+    addIconDiv.innerHTML = addIcon;
+  });
+}
